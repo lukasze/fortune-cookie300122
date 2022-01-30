216a3c486580026d328da264460e09ba6275fe49
lukasze <wyspianski.lukasz@gmail.com>
2022-01-30 09:31:47 +0100

    Inicjalizacja projektu: springi + maven

---
+++ cookie-service/src/main/java/com/example/cookieservice/CookieServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.cookieservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookieServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookieServiceApplication.class, args);
+    }
+
+}
---
+++ cookie-service/src/test/java/com/example/cookieservice/CookieServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cookieservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookieServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
